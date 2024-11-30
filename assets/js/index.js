dad1654f8eaf38ea08d3732c53093e4c28206bdb
Nathaly Vieira <nathaly.vieiracs@gmail.com>
2024-11-30 07:10:18 -0300

    final project

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,28 +1,5 @@
-/*=============== SERVICES MODAL ===============*/
-const modal = document.querySelectorAll('.services__modal'),
-        modalButton = document.querySelectorAll('.services__button'),
-        modalClose = document.querySelectorAll('.services__modal-close')
-
-let activeModal = (modalClick) => {
-    modal[modalClick].classList.add('active-modal')
-}
-
-modalButton.forEach((modalButton, i) => {
-    modalButton.addEventListener('click', () => {
-        activeModal(i) 
-    })
-})
-
-modalClose.forEach((modalClose) => {
-    modalClose.addEventListener('click', () => {
-        modal.forEach((modalRemove) => {
-            modalRemove.classList.remove('active-modal')
-        })
-    })
-})
-
-/*=============== SWIPER TESTIMONIAL ===============*/
-const swiperTestimonial = new Swiper('.team__swiper', {
+/*=============== SWIPER TEAM ===============*/
+const swiperTeam = new Swiper('.team__swiper', {
     loop: true,
     spaceBetween: 32,
     grabCursor: true,
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,25 @@
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-if(navToggle){
-    navToggle.addEventListener('click', () =>{
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
     })
 }
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-if(navClose){
-    navClose.addEventListener('click', () =>{
+if (navClose) {
+    navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
     })
 }
 
-/*=============== CHANGE BACKGROUND HEADER ===============*/
-const bgHeader = () =>{
-    const header = document.getElementById('header')
-    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-    this.scrollY >= 50 ? header.classList.add('bg-header') 
-                       : header.classList.remove('bg-header')
-}
-window.addEventListener('scroll', bgHeader)
-
-/*=============== DARK LIGHT THEME ===============*/ 
+/*=============== DARK LIGHT THEME ===============*/
 const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
 const iconTheme = 'ri-sun-line'
@@ -43,9 +34,9 @@ const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moo
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
 }
 
 // Activate / deactivate the theme manually with the button
